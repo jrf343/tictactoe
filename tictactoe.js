7df754104b4f8b146e0fce4f50be2012980abd14
Julia Frances <104040854+jrf343@users.noreply.github.com>
2023-02-09 14:39:25 -0500

    edit tictactoe.js to add functionality for marking boxes and switching turns

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,86 @@
-const container = document.querySelector('#container');
 
-const playerFactory = (name) => {
-    const getName = () => name;
+let players = [];
+let activePlayer = {};
 
-    return {getName};
-}
+const formDisplay = document.querySelector('.formDisplay');
+const newGameButton = document.querySelector('#newGame');
+newGameButton.addEventListener('click', () => {
+    formDisplay.style.display = 'block';
+});
+
+const submitButton = document.querySelector('#submitButton');
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const playerOneName = document.querySelector('#playerOne').value;
+    const playerTwoName = document.querySelector('#playerTwo').value;
+
+    const playerX = playerFactory(playerOneName, 'X');
+    const playerO = playerFactory(playerTwoName, 'O');
+    players.push(playerX);
+    players.push(playerO);
+
+    activePlayer = players.at(0);
+
+    const playerOneHeading = document.querySelector('#playerOneHead');
+    playerOneHeading.textContent = playerX.name;
+
+    const playerTwoHeading = document.querySelector('#playerTwoHead');
+    playerTwoHeading.textContent = playerO.name;
+
+    formDisplay.style.display = 'none';
+    document.querySelector("form").reset();
+});
+
+const playerFactory = (name, mark) => {
+
+    return{
+        name,
+        mark,
+    };
+};
 
 const gameBoard = (() => {
-    const boardArray = ['x', 'o', 'x', 'x', 'o', 'o', 'x', 'o', 'x'];
 
-    for (const index in boardArray) {
-        let box = document.createElement("div");
-        box.textContent = boardArray[index];
-        container.appendChild(box).className = "box";
-    }
+    const boardArray = ['', '', '', '', '', '', '', '', ''];
+
+    const container = document.querySelector('#container');
+
+    const generateBoard = () => {
+        for (const index in boardArray) {
+            let box = document.createElement("div");
+            box.textContent = boardArray[index];
+            container.appendChild(box).className = "box";
+        }
+    };
+
+    return{
+        generateBoard,
+    };
+
 })();
+
+gameBoard.generateBoard();
+
+const gameFlow = (() => {
+
+    const switchTurn = () => {
+        if ( activePlayer.mark === 'X' ) {
+            activePlayer = players.at(1);
+        } else {
+            activePlayer = players.at(0);
+        }
+    };
+
+    return{
+        switchTurn,
+    };
+
+})();
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        box.textContent = activePlayer.mark;
+        gameFlow.switchTurn();
+    })
+});
